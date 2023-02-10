712cdbcae661e4866055de794754c107b32f3e92
Subhajit Mondal <subhajit@turbot.com>
2023-02-10 08:07:07 +0530

    Add column endpoint_applications to list the installed applications in a computer. Closes #8 (#9)

--- vanta/table_vanta_computer.go
+++ vanta/table_vanta_computer.go
@@ -2,7 +2,9 @@ package vanta
 
 import (
 	"context"
+	"strings"
 
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
@@ -20,6 +22,7 @@ func tableVantaComputer(ctx context.Context) *plugin.Table {
 		},
 		Columns: []*plugin.Column{
 			{Name: "owner_name", Type: proto.ColumnType_STRING, Description: "The name of the workstation owner.", Transform: transform.FromField("Owner.DisplayName")},
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "A unique Vanta generated identifier of the computer.", Transform: transform.FromField("Data.Id")},
 			{Name: "serial_number", Type: proto.ColumnType_STRING, Description: "The serial number of the workstation.", Transform: transform.FromField("Data.SerialNumber")},
 			{Name: "agent_version", Type: proto.ColumnType_STRING, Description: "The Vanta agent version.", Transform: transform.FromField("Data.AgentVersion")},
 			{Name: "os_version", Type: proto.ColumnType_STRING, Description: "The OS version of the workstation.", Transform: transform.FromField("Data.OsVersion")},
@@ -30,6 +33,7 @@ func tableVantaComputer(ctx context.Context) *plugin.Table {
 			{Name: "host_identifier", Type: proto.ColumnType_STRING, Description: "The host identifier of the workstation.", Transform: transform.FromField("Data.HostIdentifier")},
 			{Name: "last_ping", Type: proto.ColumnType_TIMESTAMP, Description: "The time when the workstation was last scanned by the Vanta agent.", Transform: transform.FromField("Data.LastPing").Transform(transform.NullIfZeroValue).Transform(transform.UnixToTimestamp)},
 			{Name: "num_browser_extensions", Type: proto.ColumnType_INT, Description: "The number of browser extensions installed in the workstation.", Transform: transform.FromField("Data.NumBrowserExtensions")},
+			{Name: "endpoint_applications", Type: proto.ColumnType_JSON, Description: "A list of applications installed on the device.", Hydrate: listVantaComputerApplications, Transform: transform.FromValue()},
 			{Name: "installed_av_programs", Type: proto.ColumnType_JSON, Description: "A list of anti-virus programs installed in the workstation.", Transform: transform.FromField("Data.InstalledAvPrograms")},
 			{Name: "installed_password_managers", Type: proto.ColumnType_JSON, Description: "A list of password managers installed in the workstation.", Transform: transform.FromField("Data.InstalledPasswordManagers")},
 			{Name: "unsupported_reasons", Type: proto.ColumnType_JSON, Description: "Specifies the reason for unmonitored computers."},
@@ -75,4 +79,48 @@ func listVantaComputers(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydr
 	}
 
 	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func listVantaComputerApplications(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	computer := h.Item.(api.Workstation)
+
+	// If the computer is unmonitored/unsupported, the Vanta agent does not monitor
+	// the application installed on that device.
+	// Hence, the API returns an error if a request is being made for an unmonitored device.
+	// Return nil, if the device is unmonitored / unsupported.
+	var unsupportedReasons api.WorkstationUnsupportedReasons
+	if computer.UnsupportedReasons != unsupportedReasons {
+		return nil, nil
+	}
+
+	// Create client
+	conn, err := getVantaAppClient(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("vanta_computer.listVantaComputerApplications", "connection_error", err)
+		return nil, err
+	}
+
+	// As of Feb 7, 2023, the query doesn't provide the paging information
+	query, err := api.ListEndpointApplications(context.Background(), conn, computer.Data.Id)
+	if err != nil {
+		plugin.Logger(ctx).Error("vanta_computer.listVantaComputerApplications", "query_error", err)
+		return nil, err
+	}
+
+	var result []string
+	for _, app := range query.Organization.EndpointApplications {
+		// Remove all entries with empty data
+		if strings.ReplaceAll(app, " ", "") == "" {
+			continue
+		}
+
+		// Remove all duplicate data
+		if !helpers.StringSliceContains(result, app) {
+			result = append(result, app)
+		}
+	}
+
+	return result, nil
 }
